7a6924fca50bfd5039f8fcfa57cfccf5927eb0ce
pthaunoo <95956442+pthaunoo@users.noreply.github.com>
2021-12-30 20:49:35 +0400

    new vali

--- JS/validation.js
+++ JS/validation.js
@@ -7,33 +7,37 @@ function validate(){
     }
 }
 
-function validateblank(){
-var fn=document.getElementById("fisrt_name").value;
-var ln=document.getElementById("last_name").value;
-var e=document.getElementById("email").value;
-var p=document.getElementById("username").value;
-var cp=document.getElementById("password").value;
+    function validateblank(){
+    var fn=document.getElementById("first_name").value;
+    var ln=document.getElementById("last_name").value;
+    var e=document.getElementById("email").value;
+    var u=document.getElementById("username").value;
+    var p=document.getElementById("password").value;
 
-if((fn.trim().length==0 || fn<1 || fn>0)&&(fn.trim().length<3)){
-    alert("Enter a First Name with alphabets only");
-    return false;
-}
-if((ln.trim().length==0 || ln<1 || ln>0)&&(ln.trim().length<3)){
-    alert("Enter a Last Name with alphabets only");
-    return false;
-}
-if((ln.trim().length==0 || ln<1 || ln>0)&&(ln.trim().length<3)){
-    alert("Enter a Username");
-    return false;
-}
-if(e.trim().length==0 ){
-    alert("Enter an email address");
-    return false;
-}
-if(p.trim().length<8){
-    alert("Enter a password that is greater than 8");
-    return false;
-}
+    if(fn.trim().length==0){
+        alert("Enter a First Name with alphabets only");
+        return false;
+    }
+    if(ln.trim().length==0){
+        alert("Enter a Last Name with alphabets only");
+        return false;
+    }
+    if(ln.trim().length==0 ){
+        alert("Enter a Username");
+        return false;
+    }
+    if(e.trim().length==0 ){
+        alert("Enter an email address");
+        return false;
+    }
+    if(u.trim().length==0 ){
+        alert("Enter an email address");
+        return false;
+    }    
+    if(p.trim().length<8){
+        alert("Enter a password that is greater than 8");
+        return false;
+    }
 
-return true;
-}
+    return true;
+    }
