a3bfde8b20b7112608167f5a2429ffa09ef91362
pthaunoo <95956442+pthaunoo@users.noreply.github.com>
2023-06-03 01:26:00 +0400

    test1

--- Login.php
+++ Login.php
@@ -1,26 +1,113 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mo-Xpense - Track It or Waste It</title>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css"/>
+    <link rel="icon" href="Images/MoIcon.ico"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-// initializing variables
-$username = "";
-$password    = "";
-    include("connection.php");
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    if($_SERVER['REQUEST_METHOD'] =="POST"){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-        $user_signin = "SELECT * FROM Users WHERE username = '$username' AND password = '$password'";
-        $output = mysqli_query($con, $user_signin);
-            if (mysqli_num_rows($output) >0 ) {
-                    $_SESSION['username'] = $username;
-                    header('location: Homepage.php');
-                    die;
-                }
-                else {
-                        header('location: usernotexist.html');
-                        die;
-                }
+</head>
+<body>
+    <header class="header">
+    <nav class="navbar navbar-style">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#iconm">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-        }   
-    
+                <a href=""><img class="logo" src="Images/Mologo.png" alt=""></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="iconm">   
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="Index.html">Home</a></li>
+                    <li><a href="Login.html">Sign In</a></li>
+                    <li><a href="Register.html">Register</a></li>
+                    <li><a href="About.html">About</a></li>
+                </ul>
+        </div> 
+        </div>
+    </nav>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-6 banner-info">
+            <h1 class="big-text">Mo-Xpense</h1>
+            <p class="para-text">Mo-Xpense helps you get just about everything managed. A smart, easy-to-use app that allows you to track and categorize your in-and-out money,
+                 create budgets that you can actually stick to. It works seamlessly across devices and platforms, available on phone, tablet, PC and Web.</p>
+        </div>
+        <div class="col-sm-6 banner-image">
+            <img src="/Images/Mologo.png" class="img-responsive" alt="">
+            <img src="/Images/mopage.png" class="img-responsive" alt="">
+        </div>
+    </div>
+</div>
+</header>
+</body>
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mo-Xpense - Track It or Waste It</title>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css"/>
+    <link rel="icon" href="Images/MoIcon.ico"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <header class="header">
+    <nav class="navbar navbar-style">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#iconm">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a href=""><img class="logo" src="Images/Mologo.png" alt=""></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="iconm">   
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="Index.html">Home</a></li>
+                    <li><a href="Login.html">Sign In</a></li>
+                    <li><a href="Register.html">Register</a></li>
+                    <li><a href="About.html">About</a></li>
+                </ul>
+        </div> 
+        </div>
+    </nav>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-6 banner-info">
+            <h1 class="big-text">Mo-Xpense</h1>
+            <p class="para-text">Mo-Xpense helps you get just about everything managed. A smart, easy-to-use app that allows you to track and categorize your in-and-out money,
+                 create budgets that you can actually stick to. It works seamlessly across devices and platforms, available on phone, tablet, PC and Web.</p>
+        </div>
+        <div class="col-sm-6 banner-image">
+            <img src="/Images/Mologo.png" class="img-responsive" alt="">
+            <img src="/Images/mopage.png" class="img-responsive" alt="">
+        </div>
+    </div>
+</div>
+</header>
+</body>
+</html>
